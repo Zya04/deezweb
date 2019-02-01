9fdb4dcb98b0f053336dae211bf250df64f3b824
Zya04 <zya.nassurally@supinternet.fr>
2019-02-01 21:49:43 +0100

    add random music in home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="hello">
-    {{msg}}
+  <div class="home">
+    <h1>Bienvuenue sur Deezweb</h1>
+    <div v-if="randomMusic" class="random-favorit-container">
+      <Card @click="removeFav" :search="randomMusic"/>
+      <button class="random-btn" @click="changeMusic">
+        <i class="fas fa-random"></i>
+        Choisir une autre musique
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from '@/components/Card';
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
   },
+  components: {
+    Card,
+  },
+  data(){
+    return {
+      favorites: [],
+      randomMusic: [],
+    }
+  },
+  created(){
+    this.favorites = JSON.parse(localStorage.getItem('favorites'));
+    this.randomMusic = this.favorites[Math.floor(Math.random() * this.favorites.length)];
+  },
+  methods:{
+     removeFav() {
+      this.favorites = this.favorites.filter(music => music.id !== id);
+      this.randomMusic = this.favorites[Math.floor(Math.random() * this.favorites.length)];
+    },
+    changeMusic() {
+      this.randomMusic = this.favorites[Math.floor(Math.random() * this.favorites.length)];
+    },
+  }
 };
 </script>
 
