1955f15811e7e1299fdf128259b14dfb738263b7
Zya04 <zya.nassurally@supinternet.fr>
2019-02-01 17:00:28 +0100

    change name of variable

--- src/services/MusicService.js
+++ src/services/MusicService.js
@@ -3,8 +3,8 @@ import fetchJsonp from 'fetch-jsonp';
 const API = 'https://api.deezer.com/search?q=';
 
 export default {
-  search(artist, filter) {
-    return fetchJsonp(`${API}${artist}&order=${filter}&output=jsonp`)
+  search(query, filter) {
+    return fetchJsonp(`${API}${query}&order=${filter}&output=jsonp`)
       .then(res => res.json())
       .catch((error) => {
         console.log(error);
