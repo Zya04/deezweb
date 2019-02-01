75420c6f50007c4ce5e37216fecbace13fb0cf7c
Zya04 <zya.nassurally@supinternet.fr>
2019-02-01 17:00:46 +0100

    can now add on localstorage

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,7 +4,8 @@
         <h1 class="title"> {{search.title}} </h1>
         <h2 class="info">{{search.artist.name}}/{{search.album.title}}</h2>
         <audio :src="`${search.preview}`" controls >ecouter</audio>
-        <button>ajouter a ma liste des favoris</button>
+        <button @click="addFav" v-if="isFav === false">ajouter a ma liste des favoris</button>
+        <button @click="removeFav" v-if="isFav === true">Enlever de ma liste des favoris</button>
     </div>
 </template>
 
@@ -14,12 +15,48 @@ export default {
     name: 'Card',
     data() {
         return {
-            card: "",
+            favsArray: "",
         }
     },
     props: {
         search: Object,        
     },
+    methods: {
+        addFav() {
+            let favs = localStorage.getItem('favorites');
+            if (favs === null) {
+                favs = [];
+                favs.push(this.search)
+            } else {
+                favs = JSON.parse(favs);
+                favs.push(this.search);
+            }
+            localStorage.setItem('favorites', JSON.stringify(favs));
+        },
+        removeFav(){
+            let favs = localStorage.getItem('favorites');
+            if (favs !== null) {
+                favs = JSON.parse(favs);
+                let indexInFav = favs.findIndex(el => el.id === this.search.id);
+                if (indexInFav !== -1){
+                    favs.splice(indexInFav, 1)
+                    localStorage.setItem('favorites', JSON.stringify(favs));
+                }
+            }
+        },
+    },
+    computed: {
+        isFav() {
+            let favs = localStorage.getItem('favorites');
+            if (favs === null) {
+                return false;
+            } else {
+                favs = JSON.parse(favs);
+                let isInFavs = favs.find(el => el.id === this.search.id);
+                return isInFavs !== undefined;
+            }
+        }
+    }
     
 }
 </script>
@@ -27,6 +64,7 @@ export default {
 <style>
 .cardMusic{
     padding: 2% 0%;
+    margin: 2%;
 }
 .title{
     font-size: 2em;
