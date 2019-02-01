b4b2b8a5b9270a03722e2659252b66750d96cc8e
Zya04 <zya.nassurally@supinternet.fr>
2019-02-01 12:50:18 +0100

    add a navbar with scss

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/SearchTitle">Search title</router-link> |
-      <router-link to="/Favorites">Favorites</router-link>
+      <nav class="navbar">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/SearchTitle">Search title</router-link> |
+        <router-link to="/Favorites">Favorites</router-link>
+      </nav>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,13 +24,44 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: white;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #99b6a9;
     }
   }
+}
+.navbar {
+    background-color: #42b983;
+    overflow: hidden;
+    margin-bottom: 3em;
+}
+.navbar > .container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+.navbar a:link,
+.navbar a:visited {
+    color: white;
+    text-decoration: none;
+    display: inline-block;
+    padding: 0.75em 1em;
+    transition: all 150ms;
+    transform: scale(1);
+}
+.navbar a:hover {
+    background-color: #128953;
+    transform: scale(1.05);
+}
+.navbar a.router-link-exact-active {
+    background-color: #128953;
+}
+.navbar a:active {
+    transition: none;
+    color: #128953;
+    background-color: #82f9c3
 }
 </style>
