385f2df27a99503b5f1136c9f7f4e8df0ba902b9
Zya04 <zya.nassurally@supinternet.fr>
2019-02-01 12:51:53 +0100

    change name of variable

--- src/components/SearchTitleScene.vue
+++ src/components/SearchTitleScene.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="searchTitle">
-        <form @submit="displayArtist">
-            <input type="text" v-model="artist">
-            <select id="filter" v-model="filter" @submit="displayArtist">
+        <form @submit="displaySongs">
+            <input type="text" v-model="query"><br>
+            <select id="filter" v-model="filter" @change="displaySongs">
                 <option value="ALBUM_ASC">Album</option>
                 <option value="ARTIST_ASC">Artiste</option>
                 <option value="TRACK_ASC">Titre</option>
                 <option value="RANKING">Popularité</option>
                 <option value="RATING_ASC">Mieux notés</option>
-            </select>
+            </select><br>
             <button >Chercher</button>
-        </form>
-        <div v-if="search.length !== 0 " class="allCard" >
-           <Card v-for="(music, index) in search" :key="index" :search="music"/>
+        </form><br>
+        <div v-if="searchResults.length !== 0 " class="allCard" >
+           <Card v-for="(item, index) in searchResults" :key="index" :search="item"/>
         </div>
         <div v-else>
             Chercher toutes les musiques que vous voulez. 
@@ -28,8 +28,8 @@ export default {
     name: 'SearchTitleScene',
     data() {
         return {
-            artist: "",
-            search: [],
+            query: "",
+            searchResults: [],
             filter: "TRACK_ASC",
         }
     },
@@ -39,14 +39,13 @@ export default {
     props: {
     },
     methods: {
-        displayArtist(artist, filter){
-            this.search = [];
-            artist = this.artist
+        displaySongs(query, filter){
+            this.searchResults = [];
+            query = this.query
             filter = this.filter
-            MusicService.search(artist, filter  ).then((res) => {
+            MusicService.search(query, filter).then((res) => {
                 res.data.map(music => {
-                    this.search.push(music)                    
-                    console.log(this.search)
+                    this.searchResults.push(music)
                 })
             })
         }
